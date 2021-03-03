0bf9aabefc8c2418c06719f2b8163dda260f72af
vicc23@gmail.com <vicc23@gmail.com>
2021-03-03 15:39:10 -0600

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+
+const Header = (props) => {
+  const { score, maxScore } = props
+  return (
+    <div>
+      <h1>Memory Card Game</h1>
+      <p>Best Score: {maxScore}</p>
+      <p>Current Score: {score}</p>
+      <p>Get 1 point by clicking on an image but don't click it more than once!</p>
+    </div>
+  );
+}
 
 function App() {
+
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+
+  const handleClick = () => {
+    setScore((prevScore) => prevScore + 1);
+  }
+
+  const handleFinish = () => {
+    if(score>maxScore){
+      setMaxScore(() => score);
+    }
+    setScore(() => 0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header score={score} maxScore={maxScore} />
+      <button onClick={handleClick}>Click card</button>
+      <button onClick={handleFinish}>Finish</button>
     </div>
   );
 }
